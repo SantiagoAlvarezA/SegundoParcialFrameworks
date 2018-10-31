c74f198a9766aa55c57545ea862b452b85314113
Santiago Alvarez <aalvarez3570@gmail.com>
2018-10-31 12:19:37 -0500

    init Parcial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListCountriesComponent } from './list-countries/list-countries.component';
+
+const appRoutes:Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'listCountries', component: ListCountriesComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ListCountriesComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
